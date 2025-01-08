880c44d3cb93bf8aa877012e77c4b63fd65ee86d
Tellox12 <manolo1234555@hotmail.com>
2025-01-07 20:43:04 -0600

    mi primer cambio

--- README.md
+++
@@ -1,2 +0,0 @@
-# EA96
-Mi primer repositorio en Git aprendi muchas cosas 
--- holamundo.c
+++ holamundo.c
@@ -4,7 +4,21 @@
 #include<stdio.h>
 
 void main(){
+
+  char zr[100];
+  float hi;
+  int a=15;
+
+
   printf("Hello World");
+
+  printf("Ingresa tu nombre: ");
+  scanf("%s",z);
+  for(int j=0;j<a;j++){
+
+  }
+
+
 
   return 0;
 }
